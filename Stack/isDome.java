5fabf783b248203958525c2959f9b2fa9c72b6a3
Ridham <115993280+rythm01@users.noreply.github.com>
2023-10-04 22:46:23 +0530

    DOME CHECKING CODE added in stack

---
+++ Stack/isDome.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class isDome {
+
+    public static String CheckDome(String st) {
+        if (st == "")
+            return new String("false");
+
+        Stack<String> stack = new Stack<>();
+        String ans = "true";
+        int life = 1;
+
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) == '<') {
+                if (st.charAt(++i) == '/') {
+                    if (stack.isEmpty())
+                        return new String("false");
+                    String prev = stack.pop();
+                    String curr = "";
+                    int j = i + 1;
+                    while (st.charAt(j) != '>') {
+                        curr += st.charAt(j);
+                        j++;
+                    }
+                    if (!prev.equals(curr)) {
+                        if (life == 1) {
+                            life--;
+                            ans = prev;
+                        } else
+                            ans = "false";
+                    }
+                } else if(st.charAt(++i) != '/') {
+                    String tag = "";
+                    int j = i-1;
+                    while (st.charAt(j) != '>') {
+                        tag += st.charAt(j);
+                        j++;
+                    }
+                    stack.push(tag);
+                }
+            }
+        }
+        if (!stack.isEmpty())
+            return new String("false");
+        else
+            return ans;
+    }
+
+    public static void main(String[] args) {
+        String st;
+        Scanner sc = new Scanner(System.in);
+        st = sc.nextLine();
+
+        System.out.println(CheckDome(st));
+    }
+}
